5124be09f05bae905aa0e6d9184f086e7ef76df0
whity-82 <everfree.main+whity82@gmail.com>
2013-04-27 13:06:06 +0900

    Revert "add logging structure."
    This reverts commit ffcc134111d6c0cbac43acec719a3fb16d622536.

--- config/system.rb
+++
@@ -1,5 +0,0 @@
-# ログファイルへのパス。STDOUTとSTDERRを指定することも可能です。
-LOG_FILE=STDERR
-
-# デバッグモード
-DEBUG_MODE=true
--- game/game_window.rb
+++ game/game_window.rb
@@ -1,20 +1,11 @@
 #coding:utf-8
-require File.dirname(__FILE__) + "/require.rb"
-
-# the logger
-$log = nil
+require "./game/require.rb"
 
 class GameWindow < Chingu::Window
 
   def initialize
     super(WINDOW_WIDTH, WINDOW_HEIGHT, false)
     self.caption = "Bande"
-
-    if $log.nil?
-      $log = Logger.new(LOG_FILE)
-      $log.level = (DEBUG_MODE) ? Logger::DEBUG : Logger::ERROR
-    end
-
   end
 
   def setup
