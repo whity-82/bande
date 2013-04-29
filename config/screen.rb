d94c72a9f0434382a9d89b480b6bf59a8dfa7458
whity-82 <everfree.main+whity82@gmail.com>
2013-04-29 16:41:15 +0900

    changed font size and made it enable to change font in config.

--- config/screen.rb
+++ config/screen.rb
@@ -1,8 +1,11 @@
+#coding:utf-8
+
 # ウィンドウの幅
 WINDOW_WIDTH= 800
 # ウィンドウの高さ
 WINDOW_HEIGHT= 640
 
 # 標準フォントサイズ
-FONT_SIZE = 16
-
+FONT_SIZE = 20
+# 標準フォント(等幅フォントを指定してください)
+FONT_NAME = "VL-Gothic-Regular"
--- game/game_window.rb
+++ game/game_window.rb
@@ -7,14 +7,25 @@ $log = nil
 class GameWindow < Chingu::Window
 
   def initialize
-    super(WINDOW_WIDTH, WINDOW_HEIGHT, false)
-    self.caption = "Bande"
 
     if $log.nil?
       $log = Logger.new(LOG_FILE)
       $log.level = (DEBUG_MODE) ? Logger::DEBUG : Logger::ERROR
     end
 
+    font_filename = File.dirname(__FILE__) + "/fonts/" + FONT_NAME + ".ttf"
+    if File.exists? font_filename
+      $log.debug "font file exists"
+      Chingu::Text.font = font_filename
+    else
+      $log.debug "font file not used"
+      Chingu::Text.font = FONT_NAME
+    end
+
+    Chingu::Text.size = FONT_SIZE
+
+    super(WINDOW_WIDTH, WINDOW_HEIGHT, false)
+    self.caption = "Bande"
   end
 
   def setup
